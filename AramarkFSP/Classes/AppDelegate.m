d47ccffd9f3047225f380e60b465e8bbbab47f0e
bkendall <bkendall@MAC-FS-App-Dev2.local>
2015-03-11 15:17:05 -0400

    nslogging location of documents folder which is where the database file lives. location is now written in appdeleate

--- AramarkFSP/Classes/AppDelegate.m
+++ AramarkFSP/Classes/AppDelegate.m
@@ -146,6 +146,10 @@ static NSString * const OAuthRedirectURI        = @"sfdc://success";
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    #if TARGET_IPHONE_SIMULATOR
+    // where are you?
+    NSLog(@"Documents Directory: %@", [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject]);
+    #endif
     [SVProgressHUD setBackgroundColor:[UIColor blackColor]];
     [SVProgressHUD setForegroundColor:[UIColor whiteColor]];
     
