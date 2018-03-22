fe02c2a5d9b6c09d3e8de9c3b5283aaacc0906ea
bkendall <bkendall@MAC-FS-App-Dev2.local>
2018-03-22 12:02:42 -0400

    I&E 001231 20180322

--- AramarkFSP/Classes/ViewControllers/OrderListViewController/AMOrderListCell/AMBenchActiveListCell.h
+++ AramarkFSP/Classes/ViewControllers/OrderListViewController/AMOrderListCell/AMBenchActiveListCell.h
@@ -29,7 +29,7 @@
 @property (weak, nonatomic) IBOutlet UIButton *btnCheckout;
 @property (strong, nonatomic) NSDictionary *fullAssetInfoDict;
 @property (strong, nonatomic) NSString *strAssetID;
-@property (strong, nonatomic) NSString *strWOID;
+@property (strong, nonatomic) NSString *strWOID; //bkk I&E 001231 20180322
 @property (weak, nonatomic) IBOutlet UIView *viewShade;
 @property (weak, nonatomic) IBOutlet UIView *viewRight;
 
